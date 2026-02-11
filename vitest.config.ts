b13c866da2f9ed79a99ac54a78b2027dcb35e20f
theduodecim <juansebastian.012@gmail.com>
2026-02-11 18:47:27 -0300

    Add coverage configuration to Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["tests/**/*.test.ts"]
-  }
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
+  },
 });
